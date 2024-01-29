cc836f413b950e4eeebcf83ec25468eafc243870
Rakha Fatih <rakhafatihathallah@gmail.com>
2024-01-29 21:37:28 +0700

    Initial Commit

---
+++ StudyGroupML/StudyGroupMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyGroupMLApp.swift
+//  StudyGroupML
+//
+//  Created by Rakha Fatih Athallah on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyGroupMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
